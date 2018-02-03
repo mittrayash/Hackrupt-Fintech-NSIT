fb725baf7539c54a2b1691c1080bb171606bbead
Vasu Gupta <androfly.official@gmail.com>
2018-02-03 17:54:43 +0530

    Linked Customer Model with Database and added password field

--- Customer/views.py
+++ Customer/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
+from .models import Customer
+from django.shortcuts import render , get_object_or_404
+from  rest_framework.views import APIView
+from  rest_framework.response import Response
+from  rest_framework import status
+from .serializers import CustomerSerializer
 
 # Create your views here.
+
+# List all artists or create a new one
+#predict/
+class CustomerList(APIView) :
+
+    def get(self , request):
+        id = 0
+        #id = request.GET['id']
+        if id == 0 :
+            artist = Customer.objects.all()
+        else:
+            artist = Customer.objects.filter(customer_id=id).values()
+        serializer = CustomerSerializer(artist , many=True)
+        return Response(serializer.data)
